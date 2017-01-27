d4d92417eb03c35e6ea8d932d1ff0faa882e8d56
Rob Palmer <robpalmer@Robs-MacBook-Pro-2.local>
2017-01-27 09:24:53 -0500

    Assignment 11 complete

--- scripts/album.js
+++ scripts/album.js
@@ -10,10 +10,9 @@ var albumPicasso = {
         {title: 'Red' , duration: '5:01' } ,
         {title: 'Pink' , duration: '3:21' } ,
         {title: 'Magenta' , duration: '2:15'}
-        ]
+        ] 
 };
-
-var albumMarconi = {
+ var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
      label: 'EM',
@@ -25,8 +24,22 @@ var albumMarconi = {
          { title: 'Fits in your pocket', duration: '3:21'},
          { title: 'Can you hear me now?', duration: '3:14' },
          { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
+         ] 
+};
+var albumGianni = {
+     title: 'The Pasta',
+     artist: 'Guiseppi Gianni',
+     label: 'IT',
+     year: '1978',
+     albumArtUrl: 'assets/images/album_covers/03.png',
+     songs: [
+         { title: 'Hey You!', duration: '4:01' },
+         { title: 'Pasta, Pasta, Pasta', duration: '5:01' },
+         { title: 'Baccala all day', duration: '3:31'},
+         { title: 'Mangia!', duration: '8:14' },
+         { title: 'Grappa is bueno', duration: '2:15'}
+         ]
+};
 
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
@@ -40,13 +53,14 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var setCurrentAlbum = function(album) {
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
+
+var setCurrentAlbum = function(album) {
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -58,6 +72,12 @@ var setCurrentAlbum = function(album) {
  };
  
  window.onload = function() {
-     
-     setCurrentAlbum(albumPicasso);
+    setCurrentAlbum(albumPicasso);
+    var albumArray = [albumPicasso, albumMarconi , albumGianni ];
+    var index = 1;   
+    albumImage.addEventListener("click" , function (event) {
+        setCurrentAlbum(albumArray[index]);
+        index++;
+        if(index == albumArray.length){index = 0;}
+        });
  };
